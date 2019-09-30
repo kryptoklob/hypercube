7c0752802e71802c1e5d6d33b765b991d068efc2
Klob <ditchfieldcaleb@gmail.com>
2019-09-30 18:59:19 -0400

    move routines and utilities into their own folders

--- src/includes.h
+++ src/includes.h
@@ -1,13 +1,13 @@
 // Variables
 #include "variables.h"
 
-// Support functions
-#include "add_glitter.h"
-#include "clear_all.h"
-#include "fill_solid_all.h"
+// Support/util functions
+#include "util/add_glitter.h"
+#include "util/clear_all.h"
+#include "util/fill_solid_all.h"
 
 // Display routines
-#include "rainbow_march.h"
-#include "two_sin.h"
-#include "cube_test.h"
-#include "cube_test_parallel.h"
+#include "routines/rainbow_march.h"
+#include "routines/two_sin.h"
+#include "routines/cube_test.h"
+#include "routines/cube_test_parallel.h"
